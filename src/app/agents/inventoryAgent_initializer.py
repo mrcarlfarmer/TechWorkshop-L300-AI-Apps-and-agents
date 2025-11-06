971b6beaf0e0dc456cb0512daadaeefa0ba9de7f
mrcarlfarmer <mrcarlfarmer@gmail.com>
2025-11-06 12:21:51 +0000

    refactor: Enhance agent creation logic to check for existing agents and update accordingly

--- src/app/agents/inventoryAgent_initializer.py
+++ src/app/agents/inventoryAgent_initializer.py
@@ -31,12 +31,31 @@ toolset = ToolSet()
 toolset.add(functions)
 project_client.agents.enable_auto_function_calls(tools=functions)
 
+ # Create the agent using a specific deployment, name, instructions, and toolset
+agent_name="Zava Inventory Agent"
+
 with project_client:
-    # Create an agent with the Bing Grounding tool
-    agent = project_client.agents.create_agent(
-        model=os.getenv("AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"),  # Model deployment name
-        name="Zava Inventory Agent",  # Name of the agent
-        instructions=IA_PROMPT,  # Instructions for the agent
-        toolset=toolset
-    )
-    print(f"Created agent, ID: {agent.id}")
+    agent_exists = False
+    if agent_id:
+        # Check if agent exists.
+        agent = project_client.agents.get_agent(agent_id)
+        print(f"Retrieved existing agent, ID: {agent.id}")
+        agent_exists = True
+    
+    if agent_exists:
+        agent = project_client.agents.update_agent(
+            agent_id=agent.id,
+            model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],  # Model deployment name
+            name=agent_name,  # Name of the agent
+            instructions=IA_PROMPT,  # Updated instructions for the agent
+            # toolset=toolset
+        )
+        print(f"Updated agent, ID: {agent.id}")
+    else:
+        agent = project_client.agents.create_agent(
+            model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],  # Model deployment name
+            name=agent_name,  # Name of the agent
+            instructions=IA_PROMPT,  # Instructions for the agent
+            # toolset=toolset
+        )
+        print(f"Created agent, ID: {agent.id}")
